9d0da5285107ebaac9c5464a39609e5d8951955c
sahnizohaib <sahnizohaib@gmail.com>
2022-03-06 17:15:34 +0500

    Revert "Branch First Commit"
    This reverts commit cdf01ef4b5b2ea97cf19c747d92b66de5988d341.

--- app/src/main/java/com/app/sample/learning/MainActivity.kt
+++ app/src/main/java/com/app/sample/learning/MainActivity.kt
@@ -2,17 +2,10 @@ package com.app.sample.learning
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 
 class MainActivity : AppCompatActivity() {
-
-    private val TAG="MainLog:"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
-
-    private fun methodBranch1(){
-        Log.d(TAG, "methodBranch1: ")
     }
 }
