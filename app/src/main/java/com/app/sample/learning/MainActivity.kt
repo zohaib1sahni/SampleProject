4eaca92abc26bd78df02091b23d87e3ce1a8326e
sahnizohaib <sahnizohaib@gmail.com>
2022-03-06 17:44:00 +0500

    Branch Second Commit

--- app/src/main/java/com/app/sample/learning/MainActivity.kt
+++ app/src/main/java/com/app/sample/learning/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.app.sample.learning
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private val TAG = "MainLogger:"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    private fun branchAfterRevertMethod1() {
+        Log.d(TAG, "branchAfterRevertMethod1: ")
     }
 }
